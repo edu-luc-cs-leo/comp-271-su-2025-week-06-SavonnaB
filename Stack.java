e6f28e082dc8b0de8e20a4a680ec5d5edc2364e7
SavonnaB <143576792+SavonnaB@users.noreply.github.com>
2025-07-09 18:33:09 +0000

    finish toString method in Stack.java

--- Stack.java
+++ Stack.java
@@ -1,7 +1,7 @@
 public class Stack extends DynamicArray implements Comparable<Stack>, Xifo<String>, Lifo<String> {
 
     public int compareTo(Stack other) {
-        return -1234;
+        return this.size() - other.size(); //SB: this logic compares boths stacks and returns postive/negative or 0.
     } // method compareTo
 
     public String peek() {
@@ -15,7 +15,16 @@ public class Stack extends DynamicArray implements Comparable<Stack>, Xifo<Strin
     } // method pop
 
     public String toString() {
-        return null;
+        if (this.size() == 0) return "[Empty Stack]"; //SB: outputs string if size equals 0.
+
+    String result = "Top -> "; // SB: pointer to string result
+    for (int i = this.size() - 1; i >= 0; i--) {
+        result += this.get(i); //SB: adds current item to result
+        if (i > 0) {
+            result += " | "; //SB: added to separate items (maybe this wasn't neccessary but added it to make output cleaner)
+        }
+    }
+    return result;
     } // method toString
 
     public void push(String e) {
