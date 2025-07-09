09e225895f68caf20530d5887ee18bf1681616e3
SavonnaB <143576792+SavonnaB@users.noreply.github.com>
2025-07-09 18:49:12 +0000

    made slight changes per readme file

--- Stack.java
+++ Stack.java
@@ -1,27 +1,27 @@
 public class Stack extends DynamicArray implements Comparable<Stack>, Xifo<String>, Lifo<String> {
 
     public int compareTo(Stack other) {
-        return this.size() - other.size(); //SB: this logic compares boths stacks and returns postive/negative or 0.
+        return this.getOccupancy() - other.getOccupancy(); //SB: this logic compares boths stacks and returns postive/negative or 0.
     } // method compareTo
 
     public String peek() {
-        if (this.size() == 0 ) return null; 
-        return this.get(this.size() - 1); //SB: this piece should return element at the top of the stack without removal like a "preview"
+        if (this.getOccupancy() == 0) return null; 
+        return this.underlying[this.getOccupancy() - 1]; //SB: this piece should return element at the top of the stack without removal like a "preview"
     } // method peek
 
     public String pop() {
-        if (this.size() == 0) return null;
-        return this.remove(this.size() - 1); //SB: similar to the peek method, this should remove the last element and return the stack
+        if (this.getOccupancy() == 0) return null;
+        return this.remove(this.getOccupancy() - 1); //SB: similar to the peek method, this should remove the last element and return the stack
     } // method pop
 
     public String toString() {
-        if (this.size() == 0) return "[Empty Stack]"; //SB: outputs string if size equals 0.
+        if (this.getOccupancy() == 0) return "[Empty Stack]"; //SB: outputs string if size equals 0.
 
     String result = "Top -> "; // SB: pointer to string result
-    for (int i = this.size() - 1; i >= 0; i--) {
-        result += this.get(i); //SB: adds current item to result
+    for (int i = this.getOccupancy() - 1; i >= 0; i--) {
+        result += this.underlying[i]; //SB: adds new value to list
         if (i > 0) {
-            result += " | "; //SB: added to separate items (maybe this wasn't neccessary but added it to make output cleaner)
+            result += " | "; //SB: separates items (may not be necessary)
         }
     }
     return result;
